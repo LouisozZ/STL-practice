2632cbd2fbb56dd7d0dde1a2b2b70884e1474827
LouisozZ <qifan@outlook.com>
2019-04-16 16:51:16 +0800

    为 Add_Build.sh 添加 recover cppfile 功能

--- Add_Build.sh
+++ Add_Build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 FILE_EXIST=0
+OPTION=
+TARGETNAME=
 function usage(){
     echo -e "usage : 
     ./Add_Build.sh <TARGET>\t\tbuild file whose name is TARGET.cpp and creat a target file named TARGET and then run it
@@ -9,34 +11,50 @@ function usage(){
 }
 #检测是否是clean
 if [ $# == 1 ]; then
-    if [ $1 == clean ]; then
+    TARGETNAME=$1
+    if [ ${TARGETNAME} == "clean" ]; then
         cd build
         make clean
         exit 0
-    elif [ -f src/$1.cpp ]; then
+    elif [ -f src/${TARGETNAME}.cpp ]; then
         #检测文件是否存在
         FILE_EXIST=1
     else
         usage
     fi
 elif [ $# == 2 ]; then 
-    if [ ! -e ./bin/$2 ]; then
+    TARGETNAME=$2
+    OPTION=$1
+    if [ ${OPTION} != "-u" ] && [ ! -e ./bin/${TARGETNAME} ]; then
         usage
-        echo -e "target file $2 is not existed!\nexit..."
+        echo -e "target file ${TARGETNAME} is not existed!\nexit..."
         exit 0
     fi
-    case $1 in
+    case $OPTION in
         "-r")
-            ./bin/$2
+            ./bin/${TARGETNAME}
             exit 0
             ;;
         "-d")
-            rm ./bin/$2
-            rm ./src/$2.cpp
-            deleteline=`grep -n "$2" ./src/CMakeLists.txt | awk 'BEGIN {FS=":"} {print $1}'`
+            rm ./bin/${TARGETNAME}
+            mv ./src/${TARGETNAME}.cpp ./deletedcode
+            deleteline=`grep -n "${TARGETNAME}" ./src/CMakeLists.txt | awk 'BEGIN {FS=":"} {print $1}'`
             echo -e "while delete line ${deleteline} in ./src/CMakeLists.txt"
             sed -i "${deleteline}d" ./src/CMakeLists.txt
             exit 0
+            ;;
+        "-u")
+            if [ ! -e ./deletedcode/${TARGETNAME}.cpp ]; then
+                echo -e "there is no file '${TARGETNAME}.cpp' in ./deletedcode"
+                exit 0
+            fi
+            read -p "Do you want to recover file ${TARGETNAME}.cpp in ./deletedcode? [Y/n]" recover
+            if [ ${recover} == "Y" ] || [ ${recover} == "y" ]; then
+                mv ./deletedcode/${TARGETNAME}.cpp ./src/
+            else
+                echo -e "exit..."
+                exit 0
+            fi
             ;;
         *)
             usage
@@ -50,30 +68,30 @@ fi
 
 #检测文件是否存在
 cd src
-if [ ${FILE_EXIST} == 0 ]; then
-    echo -e "$1.cpp not existed ! \nexit build...\n"
+if [ ${OPTION} != "-u" ] && [ ${FILE_EXIST} == 0 ]; then
+    echo -e "${TARGETNAME}.cpp not existed ! \nexit build...\n"
     exit 0
 fi
 
 # 寻找要编译的目标文件是否已经在编译列表里面
-grep "$1.cpp" CMakeLists.txt >/dev/null
+grep "${TARGETNAME}.cpp" CMakeLists.txt >/dev/null
 
 if [ $? == 0 ]; then 
 # 找到了一行，则表示已存在，所以不添加
-    echo -e "target $1 have been existed!"
+    echo -e "target ${TARGETNAME} have been existed!"
 else
 # 否则在最后一行添加
-    sed -i "\$aADD_EXECUTABLE($1 $1.cpp)" CMakeLists.txt
-    echo -e "add \"ADD_EXECUTABLE($1 $1.cpp)\" into src/CMakeLists.txt...\n"
+    sed -i "\$aADD_EXECUTABLE(${TARGETNAME} ${TARGETNAME}.cpp)" CMakeLists.txt
+    echo -e "add \"ADD_EXECUTABLE(${TARGETNAME} ${TARGETNAME}.cpp)\" into src/CMakeLists.txt...\n"
 fi
 
-echo -e "now to build $1..."
+echo -e "now to build ${TARGETNAME}..."
 cd ../build && cmake .. && make
 if [ $? == 0 ]; then
     echo "--------------------------------------------------"
-    echo "file : PROJECT_SOURCE_DIR/src/$1.cpp"
+    echo "file : PROJECT_SOURCE_DIR/src/${TARGETNAME}.cpp"
     echo -e "vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv\n"
-    cd ../bin && ./$1
+    cd ../bin && ./${TARGETNAME}
     echo -e "\n^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
     echo "--------------------------------------------------"
 fi
--- src/new_sh_test.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    cout << "the sh file can work correctly!" << endl;
-    return 0;
-}
