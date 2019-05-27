0c3d9d05c0675c592dda669770f918322c02c603
LouisozZ <qifan@outlook.com>
2019-05-27 16:20:37 +0800

    1.修改上下标为html格式； 2.完成仿函数以及关联式容器剩余笔记

--- src/syntax_test.cpp
+++ src/syntax_test.cpp
@@ -11,5 +11,12 @@ int main(int argc, char *argv[])
 
     result = condition ? 1:0;
     cout << result << endl;
+
+    int a = 3;
+    int &b = a;
+    int &c = b;
+
+    cout << "value\ta:" << a << "   b:" << b << "   c:" << c <<endl;
+    cout << "address\ta:" << (void*)(&a) << "   b:" << (void*)(&c) << "   c:" << (void*)(&c) <<endl;
     return 0;
 }
