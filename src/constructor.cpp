edea78cf7ed0a514088c9b34d3ebbb41c8adffe1
LouisozZ <qifan@outlook.com>
2019-06-18 17:14:47 +0800

    updata

--- src/constructor.cpp
+++ src/constructor.cpp
@@ -1,5 +1,34 @@
 #include <iostream>
+#include <memory>
 using namespace std;
+
+class D{
+    public:
+    D(){std::cout << "D constructor" << std::endl;}
+    virtual ~D(){std::cout << "D distructor" << std::endl;}
+    virtual void function() = 0;
+};
+
+class B:virtual public D{
+    public:
+    B(){std::cout << "B constructor" << std::endl;}
+    virtual ~B(){std::cout << "B distructor" << std::endl;}
+    void function(){std::cout << "B version" << std::endl;}
+};
+
+class C:virtual public D{
+    public:
+    C(){std::cout << "C constructor" << std::endl;}
+    virtual ~C(){std::cout << "C distructor" << std::endl;}
+    void function(){std::cout << "C version" << std::endl;}
+};
+
+class A: public B,public C{
+    public:
+    A(){std::cout << "A constructor" << std::endl;}
+    ~A(){std::cout << "A distructor" << std::endl;}
+    void function(){std::cout << "A version" << std::endl;}
+};
 
 class Base_c{
     public:
@@ -18,5 +47,14 @@ int main()
     //Derived_c a(6);
     //Derived_c b("123");
     Derived_c a;
-    return 0;
+    {
+        std::shared_ptr<D> d(new A); 
+        //D* d = new A;
+        d->function();
+
+        //B* b = (dynamic_cast<A*>(d);
+        B b(*dynamic_cast<A*>(d.get()));
+        b.function();
+    }
+        return 0;
 }
