8c2d762b3c2e1374e39bba5599534980fc0b7388
LouisozZ <qifan@outlook.com>
2019-06-19 22:13:42 +0800

    添加第五章实现的笔记，遗留下降低编译依赖关系的笔记未做

--- src/13inheritance.cpp
+++ src/13inheritance.cpp
@@ -6,15 +6,18 @@ class new_base{
     private:
         int m_a;
         std::string m_data;
+        virtual void my_private_print(std::string msg){std::cout << msg << std::endl;}
     public:
         new_base(int a = 1, std::string data = "base"):m_a(a),m_data(data){std::cout << "invoke base constructor" << std::endl;}
         inline virtual void show(){std::cout << "---------base class\na : " << m_a << std::endl <<"data : " << m_data << std::endl;}
         virtual ~new_base(){std::cout << "base deconstructor" << std::endl;m_data.std::string::~string();}
+        void my_print(std::string msg = "base default message"){my_private_print(msg);}
 };
 
 class new_derived:public new_base{
     private:
         int m_age;
+        virtual void my_private_print(std::string msg){std::cout << msg << std::endl << "invoked in the derived class" << std::endl;}
     public:
         new_derived(int age = 18,int a = 99,std::string data = "derived"):new_base(a,data),m_age(age){std::cout << "invoke derived constructor" << std::endl;}
         ~new_derived(){std::cout << "derived deconstructor" << std::endl;}
@@ -29,15 +32,27 @@ int main()
     new_base base1;
     new_derived derived1;
 
-    base1.show();
+    base1.my_print();
+    derived1.my_print("a new message");
 
-    derived1.show();
-    derived1.virtual_derived_show();
-    derived1.nonvirtual_derived_show();
-    std::cout << "=======================" << std::endl;
-    new_base *base_ptr = new new_derived(24);
+    new_base *b_ptr = new new_base;
+    b_ptr->my_print();
 
-    base_ptr->show();
+    delete b_ptr;
+
+    b_ptr = new new_derived;
+    b_ptr->my_print("second message");
+    delete b_ptr;
+
+    // base1.show();
+
+    // derived1.show();
+    // derived1.virtual_derived_show();
+    // derived1.nonvirtual_derived_show();
+    // std::cout << "=======================" << std::endl;
+    // new_base *base_ptr = new new_derived(24);
+
+    // base_ptr->show();
     //base_ptr->virtual_derived_show();
     //base_ptr->nonvirtual_derived_show();
 
